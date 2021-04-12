6ea7322814aa25d79d819363ca6db2bfda347ed9
Naemi4 <nrebecca@gmx.de>
2021-04-12 20:20:29 +0200

    Delete old 1.3 and rename new one

--- Sheet1/ex1_st3.cpp
+++ Sheet1/ex1_st3.cpp
@@ -6,53 +6,46 @@ using namespace std;
 
 int main()
 {
-	cout << "Welcome to the midnight equation solver"<<endl;
-	double a;
-	double b;
-	double c;
-	cout << " Enter a number a:";
-	cin >> a;
-	cout << endl;
-	cout << " Enter a number b:";
-	cin >> b;
-	cout << endl;
-	cout << " Enter a number c:";
-	cin >> c;
-	cout << endl;
-	cout << "Trying to find solutions for " << a << "x^2+" << b << "x+" << c << "=0" << endl;
-	if (a == 0) {
-		if (b == 0) {
-			if (c == 0) {
-				cout << "All values for x are possible, since 0=0 is always true" << endl; 
-			}
-			else {
-				cout << "No solution" << endl;
-			}
-		}
-		else {
-			if (c == 0) {
-				cout << "The solution is 0" << endl;
-			}
-			else {
-				cout << "The solution is " << -c / b << endl;
-			}
-		}
-	}
-	else {
-		double sol1;
-		double sol2;
-		if ((b * 4 - 4 * a * c) < 0) {
-			cout << "No real solutions" << endl;
-		}
-		else {
-			sol1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-			sol2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-			if (sol1 != sol2) {
-				cout << "The first solution is " << sol1 << ", the second solution is " << sol2 << endl;
-			}
-			else {
-				cout << "The only solution is " << sol1 << endl;
-			}
-		}
-	}
+  cout << "Welcome to the midnight formula solver\n";
+  double a,b,c;
+  cout << " Enter a number a:\n";
+  cin >> a;
+  cout << " Enter a number b:\n";
+  cin >> b;
+  cout << " Enter a number c:\n";
+  cin >> c;
+  cout << "Trying to find solutions for the equation" << a << "x^2+" << b << "x+" << c << "=0\n";
+  if (a == 0) {
+    if (b == 0) {
+      if (c == 0) {
+	cout << "All values for x are possible, since 0=0 is always true\n";
+      }
+      else {
+	cout << "No solution\n";
+      }
+    }
+    else {
+      if (c == 0) {
+	cout << "The solution is 0\n";
+      }
+      else {
+	cout << "The solution is " << -c / b << "\n";
+      }
+    }
+  }
+  else {
+    if ((b * b - 4 * a * c) < 0) {
+      cout << "No real-valued solutions\n";
+    }
+    else {
+      double sol1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+      double sol2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+      if (sol1 != sol2) {
+	cout << "The first solution is " << sol1 << ", the second solution is " << sol2 << "\n";
+      }
+      else {
+	cout << "The only solution is " << sol1 << "\n";
+      }
+    }
+  }
 }
