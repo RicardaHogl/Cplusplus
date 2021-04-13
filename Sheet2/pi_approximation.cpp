c65e89a7a1e0be575ace4a78abf1064c28b7d0ad
Naemi4 <nrebecca@gmx.de>
2021-04-13 14:04:40 +0200

    Finish ex. 2.2

--- Sheet2/pi_approximation.cpp
+++ Sheet2/pi_approximation.cpp
@@ -44,13 +44,33 @@ int main() {
 
 	/*
 	   c)
-	   double epsilon = 0.0001;
+
+		 Now reduce epsto 0.00001 and run the program again.
+		 What result do you get now?
+
+		 Answer:
+	   epsilon = 0.0001;
 	   It takes 9487 iterations to approximate Pi to: 3.14149
 	   The difference is: 9.99994e-05 < 0.0001
 
-	   double epsilon = 0.00001;
-		 The program did not stop. It runs in an endless loop, because the integer computation
-		 k*k leads to an overflow for large numbers and that leads to sum = inf.
-		 Therefore pi - piApprox never reached.
+	   epsilon = 0.00001;
+		 The program did not stop, it runs in an endless loop, because:
+		 k is an 32bit Integer, so k*k is also an 32bit integer computation.
+		 We know that the leading bit is for the sign, so the absolute value has 31bit.
+		 For k=65536, it is k*k = 4294967296 = 2^(32), so there is an overflow,
+		 which leads to k*k = 2^(32) becomes 0. Thus we get sum = 1.0/0, which C++
+		 interprets as infinity. Therefore since k=65536 it is piApprox = inf
+		 in all following loop runs, and the stopping rule pi - piApprox <= abs
+		 will never be reached.
+
+
+		 What conclusions do you draw from this with regard to the handling of
+		 foating point arithmetic?
+
+		 Answer:
+		 We find out, that in C++ the term 1/0 is seen as infinity instead of rising an error,
+		 so we have to be careful and better check if this can happen, when we create
+		 a computation with variable divisions.
+
 	 */
 }
