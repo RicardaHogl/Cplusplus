823179e82d6521fb93a25841bb678596ce48e267
Naemi4 <nrebecca@gmx.de>
2021-04-14 15:28:43 +0200

    Fix 3.3

--- Sheet3/ex3.cpp
+++ Sheet3/ex3.cpp
@@ -8,6 +8,7 @@ int main () {
   */
 
   { // a)
+    cout << "3.2 a)" << endl;
     // Compute sum over the first n natural numbers
     long unsigned int sum = 0;
     unsigned int n = 100;
@@ -19,55 +20,67 @@ int main () {
       sum += i;
       i++;
     }
-    cout << " The sum of all natural numbers from 1 to 100 is: " << sum << endl ;
+    cout << "The sum of all natural numbers from 1 to 100 is: " << sum << endl << endl;
   }
 
   { // b) Leibnitz
-     int n = 1000000;
-     double sum = 0;
-     for(int k=0; k<=n; k++){
-       if(k%2==0){
-         sum += 1.0/(2*k+1);
-       }
-       else {
-         sum -= 1.0/(2*k+1);
-       }
+    cout << "3.2 b)" << endl;
+    int n = 1000000;
+    double sum = 0;
+    for(int k=0; k<=n; k++){
+     if(k%2==0){
+       sum += 1.0/(2*k+1);
      }
+     else {
+       sum -= 1.0/(2*k+1);
+     }
+    }
 
-     cout << " The Leibnitz series for n=1000000 is: " << sum << endl ;
+     cout << "The Leibnitz series for n=1000000 is: " << sum << endl << endl;
 
   }
 
   { // c) Collatz
-    // we have just to ckeck, whether n = 4 occurs in the sequence,
-    // because we can see directly that then 4,2,1,4,2,1,... will follow
-    int max = 1000000; // max series length we look at (avoids endless loop, if we would (unexpectedly) not find 4,2,1)
-    int m;
+    cout << "3.2 c)" << endl;
+    int maxCounter = 1000000; // max series length we look at (avoids endless loop, if we would (unexpectedly) not find 4,2,1)
+    int maxLength = -1;
+    int maxN = -1;
+    int counter;
+    int getAll = true;
     int s; // start value should be visible after the for-loop
     for (s = 1; s <= 1000000; s++){ // we check starting values s=1,...,10^6
-      m = max;
+      counter = 0;
+      int lastThree[3] = {};
       long n = s;
-        while(n!=4 && m > 0){
+        while(counter <= maxCounter
+              && !(lastThree[0] == 1 && lastThree[1] == 2 && lastThree[2] == 4))
+        {
+          counter++;
           if (n%2==0) {
             n = n/2;
           }
           else {
             n = 3*n+1;
           }
-          m--;
+          lastThree[2] = lastThree[1];
+          lastThree[1] = lastThree[0];
+          lastThree[0] = n;
         }
-      if (m == 0){
-        cout << n;
-        break;
-      }
+        if (counter > maxLength){
+          maxLength = counter;
+          maxN = s;
+        }
+        if (counter > maxCounter){
+          cout << "For starting value " << s << " we could not find the sequence 4,2,1,..."
+          "at a series length of " << counter << endl;
+          getAll = false;
+          break;
+        }
     }
 
-    if (m==0){
-      cout << "For starting value " << s << " we could not find the sequence 4,2,1,..."
-      "at a series length of " << m << endl;
-    }
-    else {
-      cout << "Collatz check succeeded: Got to a 4,2,1 loop for all starting values 1,...,10^6\n";
+    if (getAll){
+      cout << "Collatz check succeeded: Got to a 4,2,1 loop for all starting values 1,...,10^6\n"
+      "The max series has length " << maxLength << " for starting value " << maxN << endl << endl;
     }
   }
 
@@ -87,7 +100,8 @@ int main () {
         idx = i;
       }
     }
-    cout << "The minimum of the array is " << min << " at index " << idx << endl;
+    cout << "3.3" << endl;
+    cout << "The minimum of the array is " << min << " at index " << idx << endl << endl;
   }
 
 
@@ -96,6 +110,8 @@ int main () {
   */
 
   {
+    cout << "3.4" << endl;
+
     int nArr[] = {10, 100};
     int len = sizeof(nArr)/sizeof(nArr[0]);
     int max = nArr[0];
@@ -122,7 +138,8 @@ int main () {
 
       delete[] arr;
 
-    cout << "For n = " << n << " expected sum is " << expectedResult[i] << " and we get sum " << sum << endl;
+    cout << "For n = " << n << " expected sum is " << expectedResult[i];
+    cout << " and we get sum " << sum << endl;
 
     }
   }
